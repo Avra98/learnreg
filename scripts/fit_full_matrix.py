05f24f63cbfec11131d1672cacdee139b817bac9
Michael McCann <michael.thompson.mccann@gmail.com>
2020-11-24 18:46:24 -0500

    Mess with parameters to try to get multisignal learning to work

--- scripts/fit_full_matrix.py
+++ scripts/fit_full_matrix.py
@@ -7,16 +7,18 @@ import torch
 import collections
 
 import learnreg
-sigma = 5e-1
 
-train, val, A = learnreg.make_data(n=100, m=100, sigma=sigma, train_size=20, val_size=2)
-train = None  # for autogen
+sigma = 5e-1
+learning_rate = 1e0
+
+train, val, A = learnreg.make_data(
+    n=100, m=100, sigma=sigma, train_size=2000, val_size=2)
 
 # x_tv = learnreg.TV_recon(val, A, beta='auto')  # todo: write this function
 
 W, W0 = learnreg.main(A, k=100, train=train,  beta0=1e-1, val=val,
-         batch_size=1, opti_opts=('SGD', dict(lr=1e-1)),
-         num_steps=5000, val_interval=10, sigma=sigma, print_interval=10,
+         batch_size=1, opti_opts=('SGD', dict(lr=learning_rate)),
+         num_steps=10000, val_interval=10, print_interval=10,
          history_length=10)
 
 # show results
